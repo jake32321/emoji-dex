83ec8d5b8c752c17e90fa31966b47ae0d156cdd9
jake32321 <jacob.reed95@siu.edu>
2019-09-29 17:36:25 -0500

    removed unneded comments

--- src/components/emoji-card.js
+++ src/components/emoji-card.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import "../static/card.css"
 
@@ -21,7 +14,7 @@ class EmojiCard extends React.Component {
     return (
       <>  
       <div className="card">
-        <img key={this.name} src={this.imgURL}/>
+        <img alt={this.readableName} key={this.name} src={this.imgURL}/>
       </div>
       </>
     )
